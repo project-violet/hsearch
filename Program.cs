aea56c10a7e2b7a6dbbac5d3fd5de5d56c4f06ad
violet-dev <violet.dev.master@gmail.com>
2021-09-07 02:56:23 +0000

    project: init project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hsearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# hsearch
+
+## Search
+
+```
+wget -qO- http://localhost:6974/?q=female:sister
+```
